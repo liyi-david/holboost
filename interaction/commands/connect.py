21e3c67ac7e9874bc64e0312df90dc4846333e05
Li Yi <liyi_math@pku.edu.cn>
2018-09-27 18:56:19 -0700

    fix (command) : missing file

---
+++ interaction/commands/connect.py
@@ -0,0 +1,18 @@
+from .command import Command
+from .result import DictResult
+
+class ConnectCommand(Command):
+
+    def __init__(self):
+        pass
+
+    def run(self, top):
+        if 'cache' not in top.namespace:
+            # create cache
+            top.namespace['cache'] = dict()
+
+        top.print('connection from client [ %s ]' % self.task.client)
+
+        # TODO session is important for further parallel optimization
+        # here 0 is only a placeholder
+        return DictResult({"session": 0})
